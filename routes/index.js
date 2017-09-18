7eb6279e854e26d11ac51e490aafcffd28ca489c
cullenm <cmarchenko@csumb.edu>
2017-09-18 00:50:22 +0000

    took url text off of page

--- routes/index.js
+++ routes/index.js
@@ -19,13 +19,13 @@ function makeApiRequest(sendBackResponseToBrowser) {
     https.get(options, function(response){
         response.setEncoding('utf8');
         response.on('data', function(chunk) {
-            console.log("received data: "); 
-            apiResponse += chunk; 
-        }); 
+            console.log("received data: ");
+            apiResponse += chunk;
+        });
         
         response.on('end', function() {
-            console.log("status code: " + this.statusCode); 
-            //console.log("Complete response: " + apiResponse); 
+            console.log("status code: " + this.statusCode);
+            //console.log("Complete response: " + apiResponse);
             /*execute callback*/
             var responseJSON = JSON.parse(apiResponse); 
             var images = responseJSON.images; 
@@ -42,9 +42,9 @@ function makeApiRequest(sendBackResponseToBrowser) {
     }); 
 }
 
-/* GET home page. */
+/* GET home page. sets picture and url on webpage */
 router.get('/', function(req, res, next) {
-  //res.render('index', { title: 'Express', className: 'CST438' });
+  
   makeApiRequest(function(imageURI){
       res.render('getty', {imageURI: imageURI});
   }); 
